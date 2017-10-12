f209adc46e0d3df618f4b78e0de475be68ba0d98
Dylan Stratton <dylanstratton@72-50-220-113.fttp.usinternet.com>
2017-10-12 11:00:52 -0500

    add back to blog button for blog posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'gatsby-link';
 import Helmet from "react-helmet";
 import Img from 'gatsby-image';
 
@@ -10,6 +11,7 @@ export default function Template({ data }) {
       <img src={ post.frontmatter.indexImage.id.src } alt={ post.frontmatter.indexImage.id.src }></img>
       {/* {post.frontmatter.indexImage && <Img />} */}
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <Link to="/blog/">Back to blog</Link>
     </div>
   );
 }
